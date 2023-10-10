8367b4655a4ae218344141121ff91a3baaa7c3c8
VladTkach <vladand2003@gmail.com>
2023-10-10 21:52:21 +0300

    added nav bar

--- frontend/src/app/shared/shared.module.ts
+++ frontend/src/app/shared/shared.module.ts
@@ -1,12 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import {RouterLink, RouterLinkActive} from "@angular/router";
 
 
 
 @NgModule({
-  declarations: [],
+    declarations: [
+        NavbarComponent
+    ],
+    exports: [
+        NavbarComponent
+    ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterLink,
+    RouterLinkActive
   ]
 })
 export class SharedModule { }
