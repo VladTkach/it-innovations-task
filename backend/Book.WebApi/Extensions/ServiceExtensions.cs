97bdb400fe3c63a366c0ac3d8e7804872ccc8df7
VladTkach <vladand2003@gmail.com>
2023-10-10 14:35:50 +0300

    added initial migration

---
+++ backend/Book.WebApi/Extensions/ServiceExtensions.cs
@@ -0,0 +1,15 @@
+﻿using Book.DAL.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace Book.WebApi.Extensions;
+
+public static class ServiceExtensions
+{
+    public static void AddBookContext(this IServiceCollection services, IConfiguration configuration)
+    {
+        var migrationAssembly = typeof(BookContext).Assembly.GetName().Name;
+        services.AddDbContext<BookContext>(options =>
+            options.UseSqlServer(configuration["ConnectionStrings:BookDBConnection"],
+                opt => opt.MigrationsAssembly(migrationAssembly)));
+    }
+}
