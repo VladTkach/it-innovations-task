5922a6f25913e7fb1f8442921ee50c9f37e87663
VladTkach <vladand2003@gmail.com>
2023-10-12 15:28:26 +0300

    added chart styling

--- frontend/src/app/modules/chart/chart-page/chart-page.component.ts
+++ frontend/src/app/modules/chart/chart-page/chart-page.component.ts
@@ -1,8 +1,7 @@
 import {Component, OnInit, ViewChild} from '@angular/core';
 import {BaseChartDirective} from "ng2-charts";
-import {ChartConfiguration, ChartData, ChartEvent, ChartType} from "chart.js";
+import {ChartData} from "chart.js";
 
-import DataLabelsPlugin from 'chartjs-plugin-datalabels';
 import {ChartService} from "../../../core/services/chart.service";
 import {BaseComponent} from "../../../core/base/base.component";
 import {takeUntil} from "rxjs";
@@ -15,33 +14,7 @@ import {takeUntil} from "rxjs";
 export class ChartPageComponent extends BaseComponent implements OnInit {
   @ViewChild(BaseChartDirective) chart: BaseChartDirective | undefined;
 
-  public barChartOptions: ChartConfiguration['options'] = {
-    responsive: true,
-    scales: {
-      x: {},
-      y: {
-        min: 0,
-      },
-    },
-    plugins: {
-      legend: {
-        display: true,
-      },
-      datalabels: {
-        anchor: 'start',
-        align: 'end',
-      },
-    },
-  };
-  public barChartType: ChartType = 'bar';
-  public barChartPlugins = [DataLabelsPlugin];
-
-  public barChartData: ChartData<'bar'> = {
-    labels: ['2006', '2007', '2008', '2009', '2010', '2011', '2012'],
-    datasets: [
-      {data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A'},
-    ],
-  };
+  public barChartData?: ChartData<'bar'>;
 
   constructor(private chartService: ChartService) {
     super();
@@ -59,7 +32,7 @@ export class ChartPageComponent extends BaseComponent implements OnInit {
           this.barChartData = {
             labels: barData.labels,
             datasets: [
-              {data: barData.data, label: 'Books per year'},
+              {data: barData.data, label: 'Books per year', backgroundColor: 'rgba(35,35,253,0.8)'},
             ],
           };
         }
